254735cb2dd059fd38024bace8735387a5b880b2
Jamci <wet2005_001@163.com>
2022-03-13 16:09:51 +0800

    提交功能 1.feature 添加倒计时功能

--- src/contentScript.js
+++ src/contentScript.js
@@ -19,18 +19,55 @@ let config = {
     characterData: true, //如果目标节点为characterData节点(一种抽象接口,具体可以为文本节点,注释节点,以及处理指令节点)时,也要观察该节点的文本内容是否发生变化
     subtree: true, //目标节点所有后代节点的attributes、childList、characterData变化
 };
+var check = [];
+var time = 5;
+var innerText = null;
+var start = false
+
+
+const countDown = () => {
+    time--
+    let btn = document.querySelector(".layui-layer-btn0");
+    if (!btn) {
+        init();
+        clearCountDown();
+        return;
+    }
+    btn.innerText = `${innerText}(${time})秒`
+    if (time <= 0) {
+        //  清空计时器
+        clearCountDown()
+        init();
+        btn.click();
+        console.log("点击成功");
+    }
+
+}
+
+const init = () => {
+    time = 5;
+    innerText = null;
+    start = false
+}
+
+const clearCountDown = () => {
+    if (check.length > 0) {
+        check.forEach((item, index) => {
+            clearInterval(item)
+            if (index === check.length - 1) {
+                check = [];
+                console.log(`${index}执行记录已清空`)
+            }
+        })
+    }
+}
 
 const mutationCallback = (mutationsList) => {
-    for(let mutation of mutationsList) {
+    for (let mutation of mutationsList) {
         let type = mutation.type;
         switch (type) {
             case "childList":
-                let dom = document.querySelector(".layui-layer.layui-layer-dialog")
-                if (dom){
-                    let btn =  document.querySelector(".layui-layer-btn0");
-                    btn.click();
-                }
-                console.log("点击成功");
+                clickBtn()
                 break;
             // case "attributes":
             //   console.log(`The ${mutation.attributeName} attribute was modified.`);
@@ -44,5 +81,22 @@ const mutationCallback = (mutationsList) => {
     }
 };
 
+const clickBtn = () => {
+    if (!start) {
+        let dom = document.querySelector(".layui-layer.layui-layer-dialog")
+        if (dom) {
+            let btn = document.querySelector(".layui-layer-btn0");
+            innerText = btn.innerText;
+            start = true;
+            // 只允许执行一个计时器
+            if (check.length === 0) {
+                check.push(setInterval((function () {
+                    countDown(innerText)
+                }), 1000));
+            }
+        }
+    }
+}
+
 var observe = new MutationObserver(mutationCallback);
-observe.observe(document.querySelector('body'), config);
+observe.observe(document.querySelector('.whaty_body'), config);
